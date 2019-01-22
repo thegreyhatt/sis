e5805be506e32e710e02a651f70f3280e7256a04
Mecel Joy Mamac <Mamac-Me@ndmu.net>
2019-01-22 19:53:10 +0800

    hello world

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,3 +17,9 @@ consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 @endsection
+
+
+@section('content header')
+<h2> Students </h2>
+@endsection
+
