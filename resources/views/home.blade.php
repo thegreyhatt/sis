6b8d2f675dada0f6cabe333e6e1ce7edee2060a5
grey_hatt <ervinbenez@gmail.com>
2019-01-22 18:28:34 +0800

    Installed AdminLTE

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title')
+	Students Information System
+@endsection
+
+@section('content_header')
+	<h1>Header</h1>
+	Hi this is content header.
+@endsection
+
+@section('content')
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+@endsection
